9b922bcd373da14d365666694ecf5aa9892ba324
Joeeazy <joeeazy34@gmail.com>
2024-03-27 11:50:07 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@
 // mongoose - connect and interact with our database
 // socket.io - enables having a real time communication
 // jsonwebtoken - create tokens
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,6 +21,9 @@ import connectToMongoDB from "./db/connectToMongDB.js";
 
 const PORT = process.env.PORT || 5000;
 
+//gives absolute paath to the root folder
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
@@ -29,6 +32,13 @@ app.use(cookieParser()); //enables access to cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+//serves static files
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // //test route
 // app.get("/", (req, res) => {
